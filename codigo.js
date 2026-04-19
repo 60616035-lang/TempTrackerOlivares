b8b8553e36ccb8260b2858a9aa4f7a02d1d40deb
Juliet Olivares Pomalaza <60616035@continental.edu.pe>
2026-04-18 20:09:16 -0500

    version 3

--- codigo.js
+++ codigo.js
@@ -1,24 +1,29 @@
-const claveApi = "2041fa1063bf4d25be4194138261804"; 
+const claveApi = "2041fa1063bf4d25be4194138261804";
 const idioma = "es";
 const inpCiudad= document.getElementById('input-ciudad')
 
+
 async function obtenerClima() {
+
 
     const ciudad = inpCiudad.value;
 
-    if(1ciudad) {
+
+    if(!ciudad) {
         alert('Por favor, ingresa una ciudad');
         return;
     }
-    
-    const apiClimaActualo = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
+   
+    const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
+
 
     const response = await fetch(apiClimaActual);
-    const data = aqwait response.json();
+    const data = await response.json();
 
-    mostrarClima(data); 
+
+    mostrarClima(data);
 }
-funtion mostrarClima(data) {
+function mostrarClima(data) {
     document.querySelector('.clima-icono').src = data.current.condition.icon;
     document.querySelector('.clima-texto').innerHTML = data.current.condition.text;
     document.querySelector('.temp').innerHTML = data.current.temp_c + '°C';
